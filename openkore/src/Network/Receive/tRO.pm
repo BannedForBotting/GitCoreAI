1c74654c1881ccfb3c16bbed96c99eb5fcbbba27
mrpond <iostream_x@hotmail.com>
2016-08-18 20:19:14 +0700

    sync_request_ex fix

--- openkore/src/Network/Receive/tRO.pm
+++ openkore/src/Network/Receive/tRO.pm
@@ -44,56 +44,93 @@ sub new {
 		'0A7B' => ['gameguard_request', 'H*', [qw(eac_key)]],
 		'0840' => ['escape_map_select', 'v a*', [qw(len mapInfo)]],
 		'09A5' => ['server_full'],
+		'085A' => ['tro_sync_request_ex'],
+		'085B' => ['tro_sync_request_ex'],
+		'085C' => ['tro_sync_request_ex'],
+		'085D' => ['tro_sync_request_ex'],
+		'085E' => ['tro_sync_request_ex'],
+		'085F' => ['tro_sync_request_ex'],
+		'0860' => ['tro_sync_request_ex'],
+		'0861' => ['tro_sync_request_ex'],
+		'0862' => ['tro_sync_request_ex'],
+		'0863' => ['tro_sync_request_ex'],
+		'0864' => ['tro_sync_request_ex'],
+		'0865' => ['tro_sync_request_ex'],
+		'0866' => ['tro_sync_request_ex'],
+		'0867' => ['tro_sync_request_ex'],
+		'0868' => ['tro_sync_request_ex'],
+		'0869' => ['tro_sync_request_ex'],
+		'086A' => ['tro_sync_request_ex'],
+		'086B' => ['tro_sync_request_ex'],
+		'086C' => ['tro_sync_request_ex'],
+		'086D' => ['tro_sync_request_ex'],
+		'086E' => ['tro_sync_request_ex'],
+		'086F' => ['tro_sync_request_ex'],
+		'0870' => ['tro_sync_request_ex'],
+		'0871' => ['tro_sync_request_ex'],
+		'0872' => ['tro_sync_request_ex'],
+		'0873' => ['tro_sync_request_ex'],
+		'0874' => ['tro_sync_request_ex'],
+		'0875' => ['tro_sync_request_ex'],
+		'0876' => ['tro_sync_request_ex'],
+		'0877' => ['tro_sync_request_ex'],
+		'0878' => ['tro_sync_request_ex'],
+		'0879' => ['tro_sync_request_ex'],
+		'087A' => ['tro_sync_request_ex'],
+		'087B' => ['tro_sync_request_ex'],
+		'087C' => ['tro_sync_request_ex'],
+		'087D' => ['tro_sync_request_ex'],
+		'087E' => ['tro_sync_request_ex'],
+		'087F' => ['tro_sync_request_ex'],
+		'0880' => ['tro_sync_request_ex'],
+		'0881' => ['tro_sync_request_ex'],
+		'0882' => ['tro_sync_request_ex'],
+		'0883' => ['tro_sync_request_ex'],
+		'0917' => ['tro_sync_request_ex'],
+		'0918' => ['tro_sync_request_ex'],
+		'0919' => ['tro_sync_request_ex'],
+		'091A' => ['tro_sync_request_ex'],
+		'091B' => ['tro_sync_request_ex'],
+		'091C' => ['tro_sync_request_ex'],
+		'091D' => ['tro_sync_request_ex'],
+		'091E' => ['tro_sync_request_ex'],
+		'091F' => ['tro_sync_request_ex'],
+		'0920' => ['tro_sync_request_ex'],
+		'0921' => ['tro_sync_request_ex'],
+		'0922' => ['tro_sync_request_ex'],
+		'0923' => ['tro_sync_request_ex'],
+		'0924' => ['tro_sync_request_ex'],
+		'0925' => ['tro_sync_request_ex'],
+		'0926' => ['tro_sync_request_ex'],
+		'0927' => ['tro_sync_request_ex'],
+		'0928' => ['tro_sync_request_ex'],
+		'0929' => ['tro_sync_request_ex'],
+		'092A' => ['tro_sync_request_ex'],
+		'092B' => ['tro_sync_request_ex'],
+		'092C' => ['tro_sync_request_ex'],
+		'092D' => ['tro_sync_request_ex'],
+		'092E' => ['tro_sync_request_ex'],
+		'092F' => ['tro_sync_request_ex'],
+		'0930' => ['tro_sync_request_ex'],
+		'0931' => ['tro_sync_request_ex'],
+		'0932' => ['tro_sync_request_ex'],
+		'0933' => ['tro_sync_request_ex'],
+		'0934' => ['tro_sync_request_ex'],
+		'0935' => ['tro_sync_request_ex'],
+		'0936' => ['tro_sync_request_ex'],
+		'0937' => ['tro_sync_request_ex'],
+		'0938' => ['tro_sync_request_ex'],
+		'0939' => ['tro_sync_request_ex'],
+		'093A' => ['tro_sync_request_ex'],
+		'093B' => ['tro_sync_request_ex'],
+		'093C' => ['tro_sync_request_ex'],
+		'093D' => ['tro_sync_request_ex'],
+		'093E' => ['tro_sync_request_ex'],
+		'093F' => ['tro_sync_request_ex'],
+		'0940' => ['tro_sync_request_ex']
 	);
-			
-	# Sync Ex Reply Array 
-	$self->{sync_ex_reply} = {
-		'085A', '085B', '085C', '085D',
-		'085E', '085F', '0860', '0861',
-		'0862', '0863', '0864', '0865',
-		'0866', '0867', '0868', '0869',
-		'086A', '086B', '086C', '086D',
-		'086E', '086F', '0870', '0871',
-		'0872', '0873', '0874', '0875',
-		'0876', '0877', '0878', '0879',
-		'087A', '087B', '087C', '087D',
-		'087E', '087F', '0880', '0881',
-		'0882', '0883', '0917', '0918',
-		'0919', '091A', '091B', '091C',
-		'091D', '091E', '091F', '0920',
-		'0921', '0922', '0923', '0924',
-		'0925', '0926', '0927', '0928',
-		'0929', '092A', '092B', '092C',
-		'092D', '092E', '092F', '0930',
-		'0931', '0932', '0933', '0934',
-		'0935', '0936', '0937', '0938',
-		'0939', '093A', '093B', '093C',
-		'093D', '093E', '093F', '0940',
-	# reply
-		'0884', '0885', '0886', '0887', 
-		'0888', '0889', '088A', '088B', 
-		'088C', '088D', '088E', '088F', 
-		'0890', '0891', '0892', '0893', 
-		'0894', '0895', '0896', '0897',
-		'0898', '0899', '089A', '089B', 
-		'089C', '089D', '089E', '089F', 
-		'08A0', '08A1', '08A2', '08A3', 
-		'08A4', '08A5', '08A6', '08A7', 
-		'08A8', '08A9', '08AA', '08AB', 
-		'08AC', '08AD', '0941', '0942', 
-		'0943', '0944', '0945', '0946', 
-		'0947', '0948', '0949', '094A', 
-		'094B', '094C', '094D', '094E', 
-		'094F', '0950', '0951', '0952', 
-		'0953', '0954', '0955', '0956', 
-		'0957', '0958', '0959', '095A', 
-		'095B', '095C', '095D', '095E', 
-		'095F', '0960', '0961', '0962', 
-		'0963', '0964', '0965', '0966', 
-		'0967', '0968', '0969', '096A'
-	};
-			
-	foreach my $key (keys %{$self->{sync_ex_reply}}) { $packets{$key} = ['sync_request_ex']; }
+						
+	#foreach my $key (keys %{$self->{sync_ex_reply}}) { $packets{$key} = ['sync_request_ex']; }
 	
 	foreach my $switch (keys %packets) {$self->{packet_list}{$switch} = $packets{$switch};	}
 	$self->{nested} = {
@@ -135,6 +172,119 @@ sub server_full {
 
 *parse_quest_update_mission_hunt = *Network::Receive::ServerType0::parse_quest_update_mission_hunt_v2;
 *reconstruct_quest_update_mission_hunt = *Network::Receive::ServerType0::reconstruct_quest_update_mission_hunt_v2;
+
+sub tro_sync_request_ex {
+	my ($self, $args) = @_;
+	$self->{sync_ex_reply} = {
+		'085A' => '0884',
+		'085B' => '0885',
+		'085C' => '0886',
+		'085D' => '0887',
+		'085E' => '0888',
+		'085F' => '0889',
+		'0860' => '088A',
+		'0861' => '088B',
+		'0862' => '088C',
+		'0863' => '088D',
+		'0864' => '088E',
+		'0865' => '088F',
+		'0866' => '0890',
+		'0867' => '0891',
+		'0868' => '0892',
+		'0869' => '0893',
+		'086A' => '0894',
+		'086B' => '0895',
+		'086C' => '0896',
+		'086D' => '0897',
+		'086E' => '0898',
+		'086F' => '0899',
+		'0870' => '089A',
+		'0871' => '089B',
+		'0872' => '089C',
+		'0873' => '089D',
+		'0874' => '089E',
+		'0875' => '089F',
+		'0876' => '08A0',
+		'0877' => '08A1',
+		'0878' => '08A2',
+		'0879' => '08A3',
+		'087A' => '08A4',
+		'087B' => '08A5',
+		'087C' => '08A6',
+		'087D' => '08A7',
+		'087E' => '08A8',
+		'087F' => '08A9',
+		'0880' => '08AA',
+		'0881' => '08AB',
+		'0882' => '08AC',
+		'0883' => '08AD',
+		'0917' => '0941',
+		'0918' => '0942',
+		'0919' => '0943',
+		'091A' => '0944',
+		'091B' => '0945',
+		'091C' => '0946',
+		'091D' => '0947',
+		'091E' => '0948',
+		'091F' => '0949',
+		'0920' => '094A',
+		'0921' => '094B',
+		'0922' => '094C',
+		'0923' => '094D',
+		'0924' => '094E',
+		'0925' => '094F',
+		'0926' => '0950',
+		'0927' => '0951',
+		'0928' => '0952',
+		'0929' => '0953',
+		'092A' => '0954',
+		'092B' => '0955',
+		'092C' => '0956',
+		'092D' => '0957',
+		'092E' => '0958',
+		'092F' => '0959',
+		'0930' => '095A',
+		'0931' => '095B',
+		'0932' => '095C',
+		'0933' => '095D',
+		'0934' => '095E',
+		'0935' => '095F',
+		'0936' => '0960',
+		'0937' => '0961',
+		'0938' => '0962',
+		'0939' => '0963',
+		'093A' => '0964',
+		'093B' => '0965',
+		'093C' => '0966',
+		'093D' => '0967',
+		'093E' => '0968',
+		'093F' => '0969',
+		'0940' => '096A'
+	};
+
+	# Debug Log
+	# message "Received Sync Ex : 0x" . $args->{switch} . "\n";
+	
+	# Computing Sync Ex - By Fr3DBr
+	my $PacketID = $args->{switch};
+	
+	# Getting Sync Ex Reply ID from Table
+	my $SyncID = $self->{sync_ex_reply}->{$PacketID};
+	
+	# Cleaning Leading Zeros
+	$PacketID =~ s/^0+//;	
+	
+	# Cleaning Leading Zeros	
+	$SyncID =~ s/^0+//;
+	
+	message ("Received Ex Packet ID : " . $PacketID . " => " . $SyncID . "\n");
+	# Converting ID to Hex Number
+	$SyncID = hex($SyncID);
+	# Dispatching Sync Ex Reply
+	$messageSender->sendReplySyncRequestEx($SyncID);
+	
+	$args->{mangle} = 2;
+}
 
 sub gameguard_request {
 	my ($self, $args) = @_;
